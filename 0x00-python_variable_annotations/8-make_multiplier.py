c300364d17cb90091cfbfa5d9c9e91ab202c118e
Abdulhafiz Gazali Ado <ahgazali108@gmail.com>
2024-02-21 22:36:57 +0000

    task 8

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -2,11 +2,12 @@
 """This module contains function make_multiplier"""
 from typing import Callable
 
+
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
-    """This function takes a float and eturns a function that 
+    """This function takes a float and eturns a function that
     multiplies a float by multiplier"""
-    
+
     def made_multiplier(n: float) -> float:
         return n * multiplier
-    
+
     return made_multiplier
