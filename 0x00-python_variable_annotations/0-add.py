eb9afd1b499c64b098a8b5ea7ac262be4c47ee56
Abdulhafiz Gazali Ado <ahgazali108@gmail.com>
2024-02-21 18:01:59 +0000

    task 0

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""This module contains my add function"""
+
+
+def add(a: float, b: float) -> str:
+    """This is a function that adds two float values"""
+    return a + b
