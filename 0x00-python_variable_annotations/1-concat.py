b72914191aed700700a99bfb1f45c910231de308
Abdulhafiz Gazali Ado <ahgazali108@gmail.com>
2024-02-21 18:17:16 +0000

    task 1

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""This module contains my concat function"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """This function takes and concatenates two strings"""
+    return str1 + str2
