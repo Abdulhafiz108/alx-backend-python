1322ad6bf487f07af5e9d7549f6ad9da4163345b
Abdulhafiz Gazali Ado <ahgazali108@gmail.com>
2024-02-21 18:40:26 +0000

    task 3

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""This file contains my to_str function"""
+
+
+def to_str(n: float) -> str:
+    """This function takes a float and returns it string representation"""
+    return str(n)
