afd982e2411608da4c7c4b52ec5b2cc856cf9560
Abdulhafiz Gazali Ado <ahgazali108@gmail.com>
2024-02-22 23:37:12 +0000

    task 3

---
+++ 0x01-python_async_function/3-tasks.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""this module contains the function task_wait_random"""
+import asyncio
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int):
+    """This function returns an asyncio.Task"""
+    return asyncio.create_task(wait_random(max_delay))
