b689977d9a77508ebdf6459d9557b7cc888c5470
Abdulhafiz Gazali Ado <ahgazali108@gmail.com>
2024-02-22 22:51:59 +0000

    task 1

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """This module contains function wait_n"""
 from typing import List
+import asyncio
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
@@ -10,9 +11,15 @@ async def wait_n(n: int, max_delay: int) -> List[float]:
     then returns a list of the delays in ascending order
     """
     delays = []
-    times = n
-    while times > 0:
-        delays.append(await wait_random(max_delay))
-        times = times - 1
 
-    return sorted(delays)
+    async def wait_and_add_delay(delay):
+        """
+        Helper function to wait for a random delay and add it to the delays list
+        """
+        random_delay = await wait_random(max_delay)
+        delays.append(random_delay)
+
+    tasks = [wait_and_add_delay(delay) for delay in range(n)]
+    await asyncio.gather(*tasks)
+
+    return delays
