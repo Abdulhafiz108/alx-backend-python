7ef3e4e77dacfd963426248dd23ec181f4f323df
Abdulhafiz Gazali Ado <ahgazali108@gmail.com>
2024-02-23 22:29:28 +0000

    task

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -5,7 +5,7 @@ from typing import Generator
 import random
 
 
-async def async_generator() -> Generator[int, None, None]:
+async def async_generator() -> Generator[float, None, None]:
     """
     This function loops 10 times and yield a random number from 1-10
     """
